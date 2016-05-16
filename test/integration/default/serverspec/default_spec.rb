75868133d9d32211691c101d07439176a134a10f
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2016-05-16 14:39:28 +0100

    [OI] Add config file templates. Add tests for config files

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,3 +3,19 @@ require 'spec_helper'
 describe package('chronograf') do
   it { should be_installed }
 end
+
+describe file('/opt/chronograf/config.toml') do
+  it { should be_file }
+  it { should be_owned_by 'chronograf' }
+  it { should be_mode 664 }
+end
+
+describe file('/etc/default/chronograf') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_mode 664 }
+end
+
+describe service('chronograf') do
+  it { should be_running }
+end
